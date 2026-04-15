29cdc1612ddf11f56cdadb1f5888af7fc0a20c13
surya762 <jbyredla@gmail.com>
2026-04-15 11:36:11 +0530

    upadte

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ import Contact from "./pages/Contact";
 import PanoramPage from "./pages/panoram";
 import NotFound from "./pages/NotFound";
 import WhatsAppFloat from "@/components/WhatsAppFloat";
-import "pannellum-react/es/css/pannellum.css";
+import "pannellum /build/pannellum.css";
 
 const queryClient = new QueryClient();
 
