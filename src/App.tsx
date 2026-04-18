c0b0edccb691ed0635dc3ab384f66e61f9e582c1
surya762 <jbyredla@gmail.com>
2026-04-18 13:18:32 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -24,6 +24,6 @@ export default function App() {
         <Route path="/3d-view" element={<ThreeDView />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
-    </<Router>
+    </Router>
   );
 }
