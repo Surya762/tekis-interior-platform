6a230779c36d143d7b5bb7138a415f7fb1c253c6
surya762 <jbyredla@gmail.com>
2026-04-18 13:11:18 +0530

    fix budget import

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,29 @@
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { Toaster as Sonner } from "@/components/ui/sonner";
-import { Toaster } from "@/components/ui/toaster";
-import { TooltipProvider } from "@/components/ui/tooltip";
-import Header from "@/components/Header";
-import Footer from "@/components/Footer";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Index from "./pages/Index";
 import Designs from "./pages/Designs";
 import DesignDetail from "./pages/DesignDetail";
 import About from "./pages/About";
+import BudgetCalculator from "./pages/BudgetCalculator";
 import Services from "./pages/Services";
 import Contact from "./pages/Contact";
 import NotFound from "./pages/NotFound";
-import WhatsAppFloat from "@/components/WhatsAppFloat";
 import ThreeDView from "./pages/ThreeDView";
-import BudgetCalculator from "./pages/BudgetCalculator";
 
-
-
-const queryClient = new QueryClient();
-
-const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      <BrowserRouter>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Index />} />
-          <Route path="/designs" element={<Designs />} />
-          <Route path="/designs/:id" element={<DesignDetail />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/budget-calculator" element={<BudgetCalculator />} />  
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/services" element={<Services />} />
-          <Route path="/3d-view" element={<ThreeDView />} />
-         <Route path="*" element={<NotFound />} />
-        </Routes>
-        <Footer />
-        <WhatsAppFloat />
-      </BrowserRouter>
-    </TooltipProvider>
-  </QueryClientProvider>
-);
-
-export default App;
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Index />} />
+        <Route path="/designs" element={<Designs />} />
+        <Route path="/designs/:id" element={<DesignDetail />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/budget-calculator" element={<BudgetCalculator />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/3d-view" element={<ThreeDView />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
