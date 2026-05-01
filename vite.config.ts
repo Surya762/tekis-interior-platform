9cd36d3573cdc8df5a63ab2be516b9959954a655
surya762 <jbyredla@gmail.com>
2026-05-01 14:58:51 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from "path";
 export default defineConfig({
   plugins: [react()],
 
-  base: "/tekis-interiors-platform/",
+  base: "/tekis-interior-platform/",
 
   resolve: {
     alias: {
