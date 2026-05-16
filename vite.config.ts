540840320bc56074810c291007f6afdd79d80ff4
surya762 <jbyredla@gmail.com>
2026-05-16 13:17:50 +0530

    remove github pages base path

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,9 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc"; 
+import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig({
   plugins: [react()],
-
-  base: "/tekis-interior-platform/",
 
   resolve: {
     alias: {
